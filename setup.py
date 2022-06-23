50dce8b570582b3779f3fc1b4f8cc52c1e7ee957
Harsh Bhatia <bhatia4@llnl.gov>
2022-06-22 20:52:24 -0700

    minor changes to setup

--- setup.py
+++ setup.py
@@ -1,39 +1,36 @@
 # Copyright (c) 2021, Lawrence Livermore National Security, LLC. All rights reserved. LLNL-CODE-827197.
 # This work was produced at the Lawrence Livermore National Laboratory (LLNL) under contract no. DE-AC52-07NA27344 (Contract 44) between the U.S. Department of Energy (DOE) and Lawrence Livermore National Security, LLC (LLNS) for the operation of LLNL.  See license for disclaimers, notice of U.S. Government Rights and license terms and conditions.
 # ------------------------------------------------------------------------------
-import sys
-from os.path import abspath, split, join
-from setuptools import setup, find_packages
 
-'''
-import platform
-dir = '{}-{}-{}.{}'.format(platform.system().lower(), platform.machine(), platform.python_version_tuple()[0], platform.python_version_tuple()[1])
-print dir
-'''
+import os
+import sys
+from setuptools import setup, find_packages
 
 # ------------------------------------------------------------------------------
 # relevant paths
-src_path = split(abspath(sys.argv[0]))[0]
-src_path = join(src_path, 'mummi_core')
+src_path = os.path.split(os.path.abspath(sys.argv[0]))[0]
+src_path = os.path.join(src_path, 'mummi_core')
 
 # ------------------------------------------------------------------------------
 setup(
     name='mummi_core',
-    description='MuMMI Core',
     version='0.9.0',
+    description='MuMMI Core',
+    url='https://github.com/mummi-framework/mummi-core',
+
     author='Harsh Bhatia, Helgi I Ingólfsson, Francesco Di Natale, Joseph Moon, Joseph R Chavez',
     author_email='hbhatia@llnl.gov, ingolfsson1@llnl.gov, dinatale3@llnl.gov, moon15@llnl.gov, chavez35@llnl.gov',
-    url='https://github.com/mummi-framework/mummi-core',
+
+    packages=find_packages(),
     entry_points={
       'console_scripts': [
-          'mummi_cmdclient = mummi_core.utils.cmdClient:main',
-          'mummi_cmdserver = mummi_core.utils.cmdServer:main',
+          'mummi_cmdclient = mummi_core.scripts.cmdClient:main',
+          'mummi_cmdserver = mummi_core.scripts.cmdServer:main',
           'mummi_monitor = mummi_core.scripts.monitor_mummi:main',
-          'mummi_bind_local_redis = mummi_core.scripts. bind_local_redis:main',
+          'mummi_bind_local_redis = mummi_core.scripts.bind_local_redis:main',
           'mummi_bind_global_redis = mummi_core.scripts.bind_global_redis:main'
       ]
     },
-    packages=find_packages(),
     install_requires=['pip>=21.2.4',
                       'pytest>=6.2.4',
                       'numpy>=1.20.2',
@@ -53,7 +50,7 @@ setup(
         "Programming Language :: Python",
         "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.7",
-    ],
+    ]
 )
 
 # ------------------------------------------------------------------------------
